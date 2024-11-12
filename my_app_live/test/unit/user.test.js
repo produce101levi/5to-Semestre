11adf8ce07d8ba258b66afe121754aee24e67f49
Levi <Daz>
2024-11-12 17:43:05 -0600

    add describe

--- my_app_live/test/unit/user.test.js
+++ my_app_live/test/unit/user.test.js
@@ -1,1 +1,8 @@
 const userModel = require("../../models/user");
+
+describe("User Model Test", () => {
+    it("should be able to return whether the user is adult", () => {
+        const user = new userModel({username: "John Doe", age: 20});
+        expect(user.$isDefault()).toBeTruthy();
+    })
+})
