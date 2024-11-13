1f1b282898cd0be09d3e0d1748cdc5b3a9ff1fe5
Levi <Daz>
2024-11-12 18:43:32 -0600

    Controller tEst

---
+++ my_app_live/test/unit/controller.test.js
@@ -0,0 +1,6 @@
+const TodoController = require("../../controllers/todo_controller");
+const TodoModel = require("../../models/todo_model");
+
+
+const httpMocks = require("node-mocks-http");
+const newTask = require("../mockups/todo_mockup.json");
