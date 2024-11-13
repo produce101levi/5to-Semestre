21ff5f98c014752922f13c21af90f594c56040d2
Levi <Daz>
2024-11-12 18:54:44 -0600

    add tests

--- my_app_live/test/unit/controller.test.js
+++ my_app_live/test/unit/controller.test.js
@@ -6,3 +6,22 @@ const httpMocks = require("node-mocks-http");
 const newTask = require("../mockups/todo_mockup.json");
 
 TodoModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+    res = httpMocks.createResponse();
+    req = httpMocks.createRequest();
+    next = null;
+})
+
+describe("Test Create method exists", () => {
+    it("should have a create method", () => {
+        expect(typeof TodoController.todoCreate).toBe("function");
+    });
+    
+    it("should call todoModel.create", () => {
+        req.body = newTodo;
+        TodoController.todoCreate(req, res, next);
+        expect(TodoModel.create).toBeCalledWith(newTodo);
+    });
+})
