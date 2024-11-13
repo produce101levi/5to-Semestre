c37184c488fb75f4fff9fe75b7870001e9246185
Levi <Daz>
2024-11-12 19:38:39 -0600

    listo

--- my_app_live/index.js
+++ my_app_live/index.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+app.set("view engine", "ejs");
 
-app.listen(3000, () => {
+app.use("", require("./routes/routes"));
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// })
+
+app.listen(3000, () => {    
     console.log("Server is running on port 3000");
 })
