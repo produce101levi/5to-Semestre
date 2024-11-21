084912798a3c9f0ae5df8afef99e71b69bb5252c
Levi <Daz>
2024-11-21 00:40:49 -0600

    commit

---
+++ PracticeTest/PracticeTest/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  PracticeTest
+//
+//  Created by Leví Diaz on 20/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Text("Testing!")
+        
+    }
+}
---
+++ PracticeTest/PracticeTestTests/PracticeTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTestTests.swift
+//  PracticeTestTests
+//
+//  Created by Leví Diaz on 20/11/24.
+//
+
+import Testing
+@testable import PracticeTest
+
+struct PracticeTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
